a15212b0504e1f06eb6e9fdf6fed23381112f609
tshemsedinov <timur.shemsedinov@gmail.com>
2018-10-08 01:37:14 +0300

    Add object properties and prototypes reflection

---
+++ JavaScript/6-descriptor.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const person = { name: 'Marcus Aurelius', born: 121 };
+
+const descriptor1 = Reflect.getOwnPropertyDescriptor(person, 'name');
+const descriptor2 = Object.getOwnPropertyDescriptor(person, 'name');
+const descriptor3 = Reflect.getOwnPropertyDescriptor(person, 'city');
+
+console.dir({ descriptor1, descriptor2, descriptor3 });
