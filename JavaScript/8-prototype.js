eab7fcc91bd72be23cf1552c2c885bb1878d746f
tshemsedinov <timur.shemsedinov@gmail.com>
2019-05-29 04:14:13 +0300

    Debug output

--- JavaScript/8-prototype.js
+++ JavaScript/8-prototype.js
@@ -1,17 +1,19 @@
 'use strict';
 
-const person = { name: 'Marcus Aurelius' };
+const person1 = { name: 'Marcus Aurelius' };
+const person2 = { name: 'Marcus Aurelius' };
 
 class Person {}
 
-const success1 = Reflect.setPrototypeOf(person, Person.prototype);
-const success2 = Object.setPrototypeOf(person, Person.prototype);
+const success1 = Reflect.setPrototypeOf(person1, Person.prototype);
+const success2 = Object.setPrototypeOf(person2, Person.prototype);
 
-console.dir({ person, success1, success2 });
+console.dir({ person1, success1 });
+console.dir({ person2, success2 });
 
 // const proto1 = Object.getPrototypeOf(person);
 
-const proto1 = Reflect.getPrototypeOf(person);
+const proto1 = Reflect.getPrototypeOf(person1);
 const proto2 = Reflect.getPrototypeOf(proto1);
 const proto3 = Reflect.getPrototypeOf(proto2);
 
